c897837a708f34322567e17ef2108e1d0c98044e
abraham konan kouame <abrahamkonany@gmail.com>
2023-02-27 14:04:42 +0000

    ok

--- exo/docFount/index.js
+++ exo/docFount/index.js
@@ -1,28 +1,30 @@
-let quest = confirm("Avez-vous plus de 18 ans");
+// let quest = confirm("Avez-vous plus de 18 ans");
+// if (quest == true) {
+//   let nbr = prompt("entrer un nombre compris de 1 à 10 ");
+//   if (parseInt(nbr) >= 1 && parseInt(nbr) <= 11) {
+//     if (parseInt(nbr) % 2 == 0) {
+//       let verif = prompt(`entrer un nombre compris entre 1 et ${nbr}`);
+//       if(parseInt(verif) % 3 == 0 && verif >=1 && verif <= verif ){
+//         alert("vous avez gagnez");
+//       }
+//       else{
+//         alert("perdu cupidon")
+//       }
+//     } else {
+//       alert("Dommage cupidon");
+//     }
+//   } else {
+//     alert("nombre non autorisé cupidon");
+//   }
+// } else {
+//   alert("vous n'etes pas autorisé");
+// }
 
-if(quest == true){
-    let nbr = prompt("entrer un nombre compris de 1 à 10 ");
+let rep = prompt("Entrer un nombre")
 
-    if(nbr >=1 && nbr <= 10){
-
-        if(parseInt(nbr) % 2 === 0){
-            let verif = prompt("entrer un nombre compris entre 1 et le nombre que vous avez entrez");
-            // if(verif >=1 && verif )
-
-        }
-        else{
-            alert("Dommage cupidon")
-        }
-    }
-    else{
-        alert("nombre non autorisé cupidon")
-    }
-
-   
-    
+if(parseInt(rep) % 2 == 0 && parseInt(rep) % 5 == 0 || parseInt(rep) % 3 == 0 && parseInt(rep) % 9 == 0 ){
+  alert("Vous avez gagnez")
 }
 else{
-    alert("vous n'etes pas autorisé");
+    alert("vous avez perdu");
 }
-
-
