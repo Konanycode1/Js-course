2cdc23d8aa17eeb82ebc68ea2ca739b45bc2ea04
kouame konan abraham <abrahamkonany@gmail.com>
2023-03-15 14:22:20 +0100

    ok

--- coursJS/class/script.js
+++ coursJS/class/script.js
@@ -4,16 +4,19 @@ class pays {
         this.nom = nom;
         this.pib = pib;
     }
-
     showpop(){
         console.log(this.pop)
     }
     getPays(){
         return `le pays est ${this.nom} et a pour polution ${this.pop} et un PIB ${this.pib}`
     }
-
+    setPays(popu, ville, pibi){
+        this.pop = popu;
+        this.nom = ville;
+        this.pib = pibi;
+    }
 }
 
 const france = new pays(70,'france', 2000)
 
-console.log(france.getPays())
+console.log(france.setPays(150, "Paris", 5000))
