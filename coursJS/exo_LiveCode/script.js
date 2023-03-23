46efc09855c3ff513a047bc19177bda31e33ae64
kouame konan abraham <abrahamkonany@gmail.com>
2023-03-23 13:09:09 +0000

    Update script.js

--- coursJS/exo_LiveCode/script.js
+++ coursJS/exo_LiveCode/script.js
@@ -1,38 +1,50 @@
-function premier(a) {
+// function premier(a) {
    
-    if(isNaN(a) || a < 0 || a > 30 ){
-        return -1;
-    }
-    else{
-        for (let i = 2; i < a; i++) {
-            if(i % a == 0){
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-    }   
-}
-let val = premier(31);
-console.log(val);
+//     if(isNaN(a) || a < 0 || a > 30 ){
+//         return -1;
+//     }
+//     else{
+//         for (let i = 2; i < a; i++) {
+//             if(i % a == 0){
+//                 return false;
+//             }
+//             else{
+//                 return true;
+//             }
+//         }
+//     }   
+// }
+// let val = premier(31);
+// console.log(val);
 
-let tab = [1,2,3,4,5];
+// let tab = [1,2,3,4,5];
 
-let add = tab.reduce((a,b)=>{
-    return Math.max(a * b) 
-});
-console.log(add)
+// let add = tab.reduce((a,b)=>{
+//     return Math.max(a * b) 
+// });
+// console.log(add)
 
-let car = "aabaa";
+// let car = "aabaa";
 
-car = car.split('').reverse().join('');
-console.log(car)
+// car = car.split('').reverse().join('');
+// console.log(car)
 
-function solution(tab){
-   for(let i = 0; i < tab.length; i++){
-        return tab[i] 
-   }
+// function solution(tab){
+//    for(let i = 0; i < tab.length; i++){
+//         return tab[i] 
+//    }
     
+// }
+// console.log(solution([3,6,-2,-5,7,3]))
+
+let ob = {
+    a: 2, b: 3, c: 4
 }
-console.log(solution([3,6,-2,-5,7,3]))
+let tab =[1,3,4,5]
+delete tab[1];
+console.log(tab)
+console.log(tab.length);
+delete ob.c;
+console.log(ob);
+
+console.log("bonjour".indexOf("b"))
